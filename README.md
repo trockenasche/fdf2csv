f5762800586b7f2614d4c544481714f3d06948fd
Murdoc Bates <trockenasche@gmail.com>
2016-04-08 17:05:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+FDF2CSV
+=========
+The Forms Data Format (FDF) is based on PDF, it uses the same syntax and has essentially the same file structure, but is much simpler than PDF, since the body of an FDF document consists of only one required object. Forms Data Format is defined in the PDF specification (since PDF 1.2). The Forms Data Format can be used when submitting form data to a server, receiving the response, and incorporating into the interactive form. It can also be used to export form data to stand-alone files that can be imported back into the corresponding PDF interactive form. Beginning in PDF 1.3, FDF can be used to define a container for annotations that are separate from the PDF document they apply to.
+
+tl;dr
+-----
+FDF (Forms Data Format) is a file format for representing form data and annotations that are contained in a PDF form.<br>
+This tool extract all information to a csv file.
+
+Usage
+=====
+    fdf2csv.py filename.fdf
+
+TODO
+====
+* Possibility to pass an alternative csv file as an argument
+* Check if there already a csv file with the same header and append the values
---
+++ fdf2csv.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+#title           :fdf2csv.py
+#description     :Extract all data from FDF file to a CSV file
+#author          :trockenasche
+#version         :0.5
+#usage           :python fdf2csv.py file.fdf
+#=================================================
+
+import sys
+import os
+import re
+import csv
+
+# check if there are a argument
+arglen = len(sys.argv)
+if not arglen == 2:
+    print("Usage: fdf2csv.py file.fdf")
+    sys.exit()
+
+# check if the file exist
+fname = sys.argv[1]
+if not os.path.isfile(fname):
+    print("Error: " + fname + " doesn't exist")
+    sys.exit()
+
+# open file
+fdf_file = open(sys.argv[1], "r")
+fdf = fdf_file.read()
+
+# replace "empty" String to an empty value
+fdf_list = re.sub("þÿ", "", fdf)
+print(fdf_list)
+
+# Where the magic happened
+pattern = re.compile('\/T\(([^)]*)\)\/V[(/]([^>)]*)')
+fdf_list = re.findall(pattern, fdf_list)
+# print(fdf_list)
+
+# separate head and values
+csv_head = []
+csv_values = []
+for i in fdf_list:
+    csv_head.append(i[0])
+    csv_values.append(i[1])
+# alternative way >>> csv_head, csv_values = zip(*fdf_list)
+
+# Set the output filename based on input file
+csv_file = re.sub("\.fdf", ".csv", fname)
+
+print("writing file", csv_file)
+
+with open(csv_file, "w") as myfile:
+    wr = csv.writer(myfile, delimiter=";", lineterminator='\n')
+    wr.writerow(csv_head)
+    wr.writerow(csv_values)
+
+
+# TODO possibility to pass an alternative csv file as an argument
+# TODO check if there already a csv file with the same header and append the values
